c1288dc1e77ef04d8e1f8b5598a22b161475d4b4
Anthony Badea <anthony.badea@cern.ch>
2022-09-20 22:40:42 +0200

    testing

--- procGrid.py
+++ procGrid.py
@@ -6,18 +6,6 @@ def main():
     for minJetPt in range(20,50,5):
         for maxNjets in range(11,17):
             confs.append({"minJetPt":minJetPt,"maxNjets":maxNjets})
-            # inDir = f"Outputs/2x5/v01/minJetPt{minJetPt}_minNjets10_maxNjets{maxNjets}"
-            # cmd = f"python matchingEff.py -i '{inDir}/user.abadea.512*.h5' -o {inDir} --minJetPt {minJetPt} --minNjets 10 --maxNjets {maxNjets}"
-            # # print(cmd)
-            # # process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-            # # process.wait()
-            # # print("Success" if process.returncode == 0 else "Failed")
-            
-            # cmd = f"python matchingEff.py -i {inDir}/eff_2x5_minJetPt{minJetPt}_minNjets10_maxNjets{maxNjets}.h5 -d /afs/cern.ch/work/a/abadea/analysis/FactoryTools/src/FactoryTools/FactoryTools/data/RPVMultijet/samples/PHYS/mc16e_GG_rpv_viaN1.list -o /eos/home-a/abadea/analysis/rpvmj/plots/matchingEfficiency/2x5/v01/minJetPt{minJetPt}_minNjets10_maxNjets{maxNjets} --minJetPt {minJetPt} --minNjets 10 --maxNjets {maxNjets} -p"
-            # print(cmd)
-            # process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-            # process.wait()
-            # print("Success" if process.returncode == 0 else "Failed")
 
     # launch jobs
     ncpu = 10
